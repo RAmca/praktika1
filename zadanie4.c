dc4162751f4f6ddf8c4001aa2ed4684b7dcdb2c4
RAmca <113855592+RAmca@users.noreply.github.com>
2026-09-14 19:11:36 +0300

    Add files via upload

---
+++ zadanie4.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+void name()
+{
+    puts("* * * * * * * * * * * * * * * * * * * * * * *");
+    puts("*                                           *");
+    puts("* тема: Разработка консольного приложения   *");
+    puts("*                                           *");
+    puts("*           Выполнил Рубцов А.А.            *");
+    puts("*                                           *");
+    puts("* * * * * * * * * * * * * * * * * * * * * * *");
+}
+
+void date()
+{
+    puts("         11.05.2008          ");
+    puts("       _   _   _   _   _   _ ");
+    puts(" | |  | | |_    | | | | | |_|");
+    puts(" | |  |_|  _|  /_ |_| |_| |_|");
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "RUS");
+
+    name();
+    date();
+
+    return 0;
+}
